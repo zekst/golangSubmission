6d13d65d56f682e481776156d920a56090e6548d
Ayush <61703695+zekst@users.noreply.github.com>
2024-08-07 13:11:12 +0530

    Typo fix (Persmissons -> Permissions)

--- FrontEnd/src/pages/WelcomePage.tsx
+++ FrontEnd/src/pages/WelcomePage.tsx
@@ -4,7 +4,7 @@ const WelcomePage = (props:{data?: Object}) => {
     const values = JSON.parse(JSON.stringify(props.data))
     return (    
         <div> 
-            {'Welcome to the portal, '+values.name+ ' You have '+values.role+' role and have '+values.Permissons+' permisssion(s) for '+ values.Departments+' department(s)'}
+            {'Welcome to the portal, '+values.name+ ' You have '+values.role+' role and have '+values.Permissions+' permisssion(s) for '+ values.Departments+' department(s)'}
         </div>
         
     );
